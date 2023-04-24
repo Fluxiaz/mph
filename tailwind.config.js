d2a1d9e011c6d25f6142792d6653880d5f16b254
BerkSKP <60089989+BerkSKP@users.noreply.github.com>
2023-04-24 11:39:23 +0200

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["C:\Users\berk9\Desktop\PACA\mph-main\mph-main.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
